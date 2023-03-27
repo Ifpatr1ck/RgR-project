bf50125af7b79814cca531f78abe8c16c61a8763
Денис Кит <Ra40k_I3_Dotk1@bk.ru>
2023-03-27 22:31:30 +0600

    A little change

--- Source.cpp
+++ Source.cpp
@@ -10,6 +10,10 @@ double e = 2.71828182845904523536; //Euler number
 double Pi = 3.1415926535; // Number Pi
 HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
 HPEN CreatePen(int fnPenStyle,int nWidth,COLORREF crColor); //Pen
+
+
+
+
 void gotoxy(int xpos, int ypos)
 {
     COORD scrn;
@@ -34,6 +38,44 @@ void ConsoleCursorVisible(bool show, short size)
 } //Visible of Console Cursor
 HWND hwnd = GetConsoleWindow(); //Descripter of Window
 HDC dc = GetDC(hwnd);//Device context - for drawing
+void grid()
+{
+    HWND hwn = GetConsoleWindow();
+    COLORREF lineColor = RGB(0, 0, 0);
+    HDC hdc = GetDC(hwn);
+    const int d = 500, k = 50, x0 = 50, y0 = 500, c = 50; // d = 350 - good. Grafic is upper because that better looking functions
+    SelectObject(hdc, CreatePen(0, 1, RGB(255, 255, 255)));
+    MoveToEx(hdc, 0, d, NULL);
+    LineTo(hdc, c * k, d);
+    MoveToEx(hdc, c, 0, NULL);
+    LineTo(hdc, c, 3 * d);
+    for (int i = 0; i < 30; i++) {
+        MoveToEx(hdc, x0 - 10, y0 - k * i, NULL);
+        LineTo(hdc, x0 + 10, y0 - k * i);
+        MoveToEx(hdc, x0 - 10, y0 + k * i, NULL);
+        LineTo(hdc, x0 + 10, y0 + k * i);
+        MoveToEx(hdc, x0 + k * i, y0 - 10, NULL);
+        LineTo(hdc, x0 + k * i, y0 + 10);
+        MoveToEx(hdc, x0 - k * i, y0 - 10, NULL);
+        LineTo(hdc, x0 - k * i, y0 + 10);
+        string s = to_string(i);
+        LPCWSTR w;
+        wstring ste = wstring(s.begin(), s.end());
+        w = ste.c_str();
+        if (i < 10) { // Drawing marks
+            TextOut(hdc, x0 - 10, y0 - k * i, w, 1);
+            TextOut(hdc, x0 + k * i, y0 + 10, w, 1);
+        }
+        else {
+            TextOut(hdc, x0 - 10, y0 - k * i, w, 2);
+            TextOut(hdc, x0 + k * i, y0 + 10, w, 2);
+        }
+        LPCWSTR w1 = L"5-3cos(x)";
+        LPCWSTR w2 = L"sqrt(e^x - 1)";
+        TextOut(hdc, 100, 80, w1, 11);
+        TextOut(hdc, 300, 60, w2, 15); // drawing eqution
+    }
+}
 void BackGround()
 {
     SelectObject(dc, GetStockObject(DC_BRUSH));
@@ -182,53 +224,27 @@ void Grafic() {
     COLORREF lineColor = RGB(0, 0, 0);
     HDC hdc = GetDC(hwn);
     const int d = 500, k = 50, x0 = 50, y0 = 500, c = 50; // d = 350 - good. Grafic is upper because that better looking functions
-    SelectObject(hdc, CreatePen(0, 1, RGB(255, 255, 255)));
-    MoveToEx(hdc, 0, d, NULL);
-    LineTo(hdc, c * k, d);
-    MoveToEx(hdc, c, 0, NULL);
-    LineTo(hdc, c, 3 * d);
-    for (int i = 0; i < 30; i++) {
-        MoveToEx(hdc, x0 - 10, y0 - k * i, NULL);
-        LineTo(hdc, x0 + 10, y0 - k * i);
-        MoveToEx(hdc, x0 - 10, y0 + k * i, NULL);
-        LineTo(hdc, x0 + 10, y0 + k * i);
-        MoveToEx(hdc, x0 + k * i, y0 - 10, NULL);
-        LineTo(hdc, x0 + k * i, y0 + 10);
-        MoveToEx(hdc, x0 - k * i, y0 - 10, NULL);
-        LineTo(hdc, x0 - k * i, y0 + 10);
-        string s = to_string(i);
-        LPCWSTR w;
-        wstring ste = wstring(s.begin(), s.end());
-        w = ste.c_str();
-        if (i < 10) {
-            TextOut(hdc, x0 - 10, y0 - k * i, w, 1);
-            TextOut(hdc, x0 + k * i, y0 + 10, w, 1);
-        }
-        else {
-            TextOut(hdc, x0 - 10, y0 - k * i, w, 2);
-            TextOut(hdc, x0 + k * i, y0 + 10, w, 2);
-        }
-        LPCWSTR w1 = L"5-3cos(x)";
-        LPCWSTR w2 = L"sqrt(e^x - 1)";
-        TextOut(hdc, 100, 80, w1, 11);
-        TextOut(hdc, 300, 60, w2, 15);
-    }
+    
     int n = 40000;
     double F1[40000], F2[40000], x1[40000], x, dx, a = 0, b = 6.28, minF1 = 20000, maxF1 = -20000, minF2 = 20000, maxF2 = -20000;
     dx = fabs(b - a) / (n - 1);//Increment of function, a,b - function interval
     x = a;
+    //drawing both funtion
+    grid();
     for (int i = 0; i < n; i++) {
         F1[i] = 5 - 3 * cos(x); //Function of Grafic #1
         F2[i] = sqrt(pow(e, x) - 1);//Function of Grafic #2
         x1[i] = x;
         x += dx;
     }
+    grid();
     for (int i = 0; i < n; i++) {
         SetPixel(hdc, c + k * x1[i], d - k * F2[i], RGB(64, 224, 208));
         MoveToEx(hdc, c + k * x1[i], d - k * F2[i], NULL);
         SetPixel(hdc, c + k * x1[i], d - k * F1[i], RGB(0, 255, 127));
         MoveToEx(hdc, c + k * x1[i], d - k * F1[i], NULL);
     }
+    grid();
 }
 double FunctionEquation(double x) {
     return tan(x) - pow(e, x + 1); // Function of Equation
@@ -243,6 +259,10 @@ double HalfDivEquation(double a, double b, double e) {
     } while (fabs(a - b) >= e);
     return c;
 }//Solving the Equation by the Half-devision Method
+double Equation(int x)
+{
+    return tan(x) - pow(e, x + 1);
+} // Equation
 void AboutME() {
     gotoxy(8, 2);
     cout << "Информация об авторе программы:";
@@ -277,10 +297,6 @@ double trapezoidalIntegral(double a, double b, int n) {
     }
     return trapezoidal_integral;
 } //Solving the Integral by the trapezoid method    
-double Equation(int x)
-{
-    return tan(x) - pow(e,x+1);
-} // Equation
 double HordEquation(double a, double b, double c)
 {
     while (fabs(Equation(b)) > c)
